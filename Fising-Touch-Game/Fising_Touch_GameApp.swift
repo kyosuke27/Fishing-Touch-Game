30a9ba6e797967e8d8d513a336fa5ef268db7a7c
Kyosuke Noguchi <kn1359028527@gmail.com>
2026-04-16 01:00:34 +0900

    initial commit

---
+++ Fising-Touch-Game/Fising_Touch_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fising_Touch_GameApp.swift
+//  Fising-Touch-Game
+//
+//  Created by kyosuke on 2026/04/16.
+//
+
+import SwiftUI
+
+@main
+struct Fising_Touch_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Fising-Touch-GameUITests/Fising_Touch_GameUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Fising_Touch_GameUITestsLaunchTests.swift
+//  Fising-Touch-GameUITests
+//
+//  Created by kyosuke on 2026/04/16.
+//
+
+import XCTest
+
+final class Fising_Touch_GameUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
