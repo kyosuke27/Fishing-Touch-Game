8780df1c713ef8414e824b8d0e6939a451b067f4
Kyosuke Noguchi <kn1359028527@gmail.com>
2026-05-09 11:05:58 +0900

    リント

--- Fising-Touch-Game/Presentation/Feature/Shop/Screen/ShopScreen.swift
+++ Fising-Touch-Game/Presentation/Feature/Shop/Screen/ShopScreen.swift
@@ -124,7 +124,7 @@ private struct ShopBaitCard: View {
     }
     
     var body: some View {
-        VStack{
+        VStack {
             HStack(spacing: 14) {
                 Circle()
                     .fill(Color.white)
@@ -170,7 +170,6 @@ private struct ShopBaitCard: View {
                     Text(bait.description)
                         .font(.subheadline)
                         .foregroundStyle(GameTheme.textSecondary)
-                    
                     
                 }
             }
