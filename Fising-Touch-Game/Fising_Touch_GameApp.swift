6480af619d842bae5380e2c9b6fa15b79ce3b7dd
Kyosuke Noguchi <kn1359028527@gmail.com>
2026-05-09 12:52:20 +0900

    ATTTrackingと設定を追加

--- Fising-Touch-Game/Fising_Touch_GameApp.swift
+++ Fising-Touch-Game/Fising_Touch_GameApp.swift
@@ -1,5 +1,6 @@
 import SwiftUI
 import GoogleMobileAds
+import AppTrackingTransparency
 
 @main
 struct Fising_Touch_GameApp: App {
@@ -10,6 +11,26 @@ struct Fising_Touch_GameApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onReceive(NotificationCenter.default.publisher(for: UIApplication.didBecomeActiveNotification)) { _ in
+                    ATTrackingManager.requestTrackingAuthorization { status in
+                        switch status {
+                        case .notDetermined:
+                            print("トラッキング許可の有無が未設定です")
+                            
+                        case .restricted:
+                            print("トラッキングが制限されています")
+                            
+                        case .denied:
+                            print("トラッキングが拒否されました")
+                            
+                        case .authorized:
+                            print("トラッキングが許可されました")
+                            
+                        @unknown default:
+                            print("不明な状態です")
+                        }
+                    }
+                }
         }
     }
 }
